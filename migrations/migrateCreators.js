987df36e60a678156429aafad94b85bbf424d34b
KilliannH <killiannhervagault@gmail.com>
2025-12-07 13:31:21 +0100

    solve binance wallet

--- migrations/migrateCreators.js
+++ migrations/migrateCreators.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const migrateCreators = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('✅ Database connected');
 
     // Récupérer tous les créateurs
